f8436ba43b6a725d364460431b630acdaa75bd20
KanstantsinNekhniadovich1992 <kanstantsin_nekhniadovich@epam.com>
2016-11-13 19:16:36 +0300

    Front Camp ES2015 module added

---
+++ js/script.js
@@ -0,0 +1,51 @@
+/*jshint esversion: 6 */
+'use strict';
+
+const URL = 'https://newsapi.org/v1/articles?source=bbc-news&apiKey=';
+const APIKEY = '6b51303bc9c44fc49fd3973b3a1e108f';
+
+getArticles();
+
+function getArticles () {
+
+    fetch(URL + APIKEY, {
+        method: 'GET'
+    }).then((response) => {
+
+        if (response.status !== 200) {
+            console.log('ajax request thrown with error: ' + response.statusText);
+            return;
+        }
+
+        response.json().then((result) => {
+            generateArticlesList(result.articles);
+        });
+    }).catch((error) => {
+        console.log('fetch error');
+    });
+}
+
+function generateArticlesList (articles) {
+
+	let articleTemplate,
+		documentFragmet,
+		articlesHolder,
+		articleHTML,
+		div,
+		compiledTemplate;
+
+	articleTemplate = document.getElementById('article-template');
+	articlesHolder = document.getElementById('articles-list');
+
+	compiledTemplate = Handlebars.compile(articleTemplate.innerHTML);
+	documentFragmet = document.createDocumentFragment();
+
+	for (let article of articles) {
+		div = document.createElement('DIV');
+		div.classList.add('article');
+		div.innerHTML = compiledTemplate(article);
+		documentFragmet.appendChild(div);
+	}
+
+	articlesHolder.appendChild(documentFragmet);
+}
