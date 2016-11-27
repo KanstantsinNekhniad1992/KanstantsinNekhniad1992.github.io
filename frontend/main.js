2a68d3029afd6bf66b8cd7227b8a692aee7663d3
KanstantsinNekhniadovich1992 <kanstantsin_nekhniadovich@epam.com>
2016-11-28 02:19:05 +0300

    add webpack configuration

---
+++ frontend/main.js
@@ -0,0 +1,33 @@
+/*jshint esversion: 6 */
+'use strict';
+
+let getNewsLink = document.getElementById('get-news-link'),
+    config = require('./config');
+
+getNewsLink.addEventListener('click', function(e) {
+    e.preventDefault();
+
+    require.ensure([], function(require) {
+
+        let articleService = require('./articles/article-service'),
+            articles,
+            articlesHolder = document.getElementById('articles-list');
+
+        require('./articles/articles.scss');
+
+        articleService.getArticles({
+            url: config.url,
+            apiKey: config.apiKey
+        }).then((result) => {
+            articles = articleService.generateArticlesList(result.articles);
+            articlesHolder.appendChild(articles);
+            getNewsLink.classList += ' hidden';
+        }).catch((error) => {
+
+            if (NODE_ENV === 'development') {
+                console.log('fetch error');
+            }
+        });
+
+    }, 'articles');
+});
---
+++ webpack.config.js
@@ -0,0 +1,56 @@
+/*jshint esversion: 6 */
+'use strict';
+
+let webpack = require('webpack'),
+    path = require('path'),
+    npmDir = path.resolve(__dirname, 'node_modules'),
+    dirSource = path.resolve(__dirname, 'frontend'),
+    dirBuild = path.resolve(__dirname, 'build');
+
+const NODE_ENV = process.env.NODE_ENV || 'development';
+
+module.exports = {
+    entry: {
+        main: dirSource + "/main.js"
+    },
+    output: {
+        path: dirBuild,
+        publicPath: '/build/',
+        filename: '[name].js'
+    },
+    module: {
+        loaders: [{
+            loader: 'babel-loader',
+            test: dirSource,
+        }, {
+            test: /\.scss$/,
+            exclude: ['/node_modules/'],
+            loaders: ["style-loader", "css-loader?sourceMap", "sass-loader?sourceMap"]
+        }]
+    },
+    watch: NODE_ENV === 'development',
+    plugins: [
+        // new webpack.optimize.UglifyJsPlugin({
+        //     compress: NODE_ENV === 'production'
+        // }),
+        new webpack.EnvironmentPlugin([
+            "NODE_ENV"
+        ]),
+        new webpack.ProvidePlugin({
+            Promise: 'imports?this=>global!exports?global.Promise!es6-promises',
+            fetch: 'imports?this=>global!exports?global.fetch!whatwg-fetch'
+        }),
+        new webpack.NoErrorsPlugin(),
+        new webpack.HotModuleReplacementPlugin(),
+        new webpack.DefinePlugin({
+            NODE_ENV: JSON.stringify(NODE_ENV)
+        })
+    ],
+    devtool: NODE_ENV === 'production' ? null : 'inline-source-map',
+    devServer: {
+        host: 'localhost',
+        port: '4000',
+        hot: true,
+        contentBase: "./",
+    }
+};
